4b3b0a0b75a3d08346d416950edd4c7a38ae6058
Olha Voronina <olha.vrnn@gmail.com>
2023-05-26 14:42:05 +0300

    feat: added main block for Latest News section

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './glob_style/index.scss';
 import RA1 from './components/RA-1';
 import RA2 from './components/RA-2';
 import RA3 from './components/RA-3';
-import RA4 from './components/RA-4';
+import RA4 from './components/RA-4/RA4.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RA1></RA1>
     <RA2></RA2>
     <RA3></RA3>
-    <RA4></RA4>
+    <RA4 
+      title='New American films on the screen of Nizami Cinema Center'
+      date='25.06.2022'
+      news='US-produced "Independence Day: Revival" at Nizami Cinema Center. "Neighbours. 2 in wartime ”films have been shown. "Independence Day: Revival" is based on a screenplay by Nicholas Wright and directed by Roland Emmerich in the genres of science fiction, adventure and war. Slogan: “We had 20 years to prepare.'
+    />
   </React.StrictMode>
 );
