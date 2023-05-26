03cf4e17844694ed7a7d1b26c36861abc9757ae7
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-05-26 19:14:15 +0300

    refactor: change bg to image, change class name of card

--- src/components/RA-1/RA1.jsx
+++ src/components/RA-1/RA1.jsx
@@ -1,17 +1,13 @@
 import styles from './RA1.module.scss';
 
+
 const RA1 = ({ movie, btnLink }) => {
-    const backgrounds = {
-        image: movie.src,
-        gradient: 'linear-gradient(180deg, rgba(29, 29, 29, 0) 15.78%, rgba(29, 29, 29, 0.78) 100%)'
-    };
 
     return <div>
-        <article className={styles.card} style={{
-            backgroundImage: `${backgrounds.gradient}, url(${backgrounds.image})`
-        }}>
-            <h2 className={styles.card__title}>{movie.title}</h2>
-            <a className={styles.card__btn} href={btnLink}>Book Now</a>
+        <article className={styles.heroCard}>
+            <img className={styles.heroCard__img} src={movie.src} alt={movie.title + ' poster'} />
+            <h2 className={styles.heroCard__title}>{movie.title}</h2>
+            <a className={styles.heroCard__btn} href={btnLink}>Book Now</a>
         </article>
     </div >;
 };
