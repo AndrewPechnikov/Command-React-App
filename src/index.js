4252cfbd4b5da538c0b9c1649816d072a96cef4f
Denis <denisnexus1994@gmail.com>
2023-05-28 19:16:17 +0300

    props added, component name changed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './glob_style/index.scss';
 import RA1 from './components/RA-1';
-import RA2 from './components/RA-2';
+import MainCard from './components/MainCard';
 import RA3 from './components/RA-3';
 import RA4 from './components/RA-4';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RA1></RA1>
-    <RA2></RA2>
+    <MainCard
+       title="The Lion King"
+       date = "20 April"
+       age = "+12"
+       languageUA ="UA"
+       languageEN = "EN"
+    ></MainCard>
     <RA3></RA3>
     <RA4></RA4>
   </React.StrictMode>
