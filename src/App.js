8bc0103b9a7ff6d4bb89c103fedbe1ef02efdbbb
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2023-05-31 16:11:35 +0300

    init: add app.js change import

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+
+import RA1 from './components/RA-1';
+import MainCard from './components/MainCard';
+import RA3 from './components/RA-3';
+import LatestNewsMainBlock from './components/RA-4/LatestNewsMainBlock';
+
+import poster from '../src/components/RA-1/card-img.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+        <MainCard
+       title="The Lion King"
+       date = "20 April"
+       age = "+12"
+       languageUA ="UA"
+       languageEN = "EN"
+    ></MainCard>
+    <RA1
+      movie={{
+        title: 'Wonder Woman 1984',
+        src: poster,
+        alt: 'Wonder Woman 1984 Poster'
+      }}
+      btnLink={'#'}
+    />
+
+    <RA3></RA3>
+    <LatestNewsMainBlock 
+      title='New American films on the screen of Nizami Cinema Center'
+      date='25.06.2022'
+      news='US-produced "Independence Day: Revival" at Nizami Cinema Center. "Neighbours. 2 in wartime ”films have been shown. "Independence Day: Revival" is based on a screenplay by Nicholas Wright and directed by Roland Emmerich in the genres of science fiction, adventure and war. Slogan: “We had 20 years to prepare.'
+    />
+    
+    </div>
+  );
+}
+
+export default App;
