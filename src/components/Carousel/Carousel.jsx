f0f0a37f31c19658e6c81efad1ce53f3b407fe85
Oleksiy Hrybach <a.a.gribach@gmail.com>
2023-06-04 23:59:36 +0300

    fix: set background by default

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -5,6 +5,8 @@ import HeroCard from 'components/HeroCard';
 import styles from './Carousel.module.scss';
 import cardStyles from '../HeroCard/HeroCard.module.scss';
 
+const imgApi = 'https://image.tmdb.org/t/p/w500/';
+
 const Carousel = ({ setBackground, activeBtn }) => {
     const [activeCard, setActiveCard] = useState(6);
     const [data, setData] = useState([]);
@@ -21,9 +23,10 @@ const Carousel = ({ setBackground, activeBtn }) => {
         const loadData = async () => {
             const movies = await getMovies(movieCategory);
             setData(movies.results);
+            setBackground(`${imgApi}${movies.results[6].poster_path}`);
         };
         loadData();
-    }, [movieCategory, setData]);
+    }, [movieCategory, setData, setBackground]);
 
     const handleCardClick = (index) => {
         setActiveCard(index === activeCard ? activeCard : index);
